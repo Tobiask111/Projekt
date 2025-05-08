8b652a5429ba38f1fcfdfe3018add5ad260a32c7
Tobiask111 <eaa24tkrin@students.eaaa.dk>
2025-05-08 20:12:24 +0200

    Failed bug fixing

--- Db/createDb.js
+++ Db/createDb.js
@@ -1,11 +1,10 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
-
 import { upload } from 'pg-upload';
-
 
 dotenv.config();
 console.log('Connecting to database', process.env.PG_DATABASE);
+
 const db = new pg.Pool({
     host:     process.env.PG_HOST,
     port:     parseInt(process.env.PG_PORT),
@@ -14,192 +13,191 @@ const db = new pg.Pool({
     password: process.env.PG_PASSWORD,
     ssl:      { rejectUnauthorized: false },
 });
+
 const dbResult = await db.query('select now()');
 console.log('Database connection established on', dbResult.rows[0].now);
 
-console.log('Recreating table Eksport_Kaffe');
-await db.query(`
-drop table if exists Eksport_Kaffe;
+// Drop og create alle tabeller
+await db.query(`drop table if exists Eksport_Kaffe;
 create table Eksport_Kaffe (
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating table Eksport_Levende_Dyr');
-await db.query(`
-drop table if exists Eksport_Levende_Dyr;
-create table Eksport_Levende_Dyr(
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+await db.query(`drop table if exists Eksport_Levende_dyr;
+create table Eksport_Levende_dyr (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating table Export_Maskiner');
-await db.query(`
-drop table if exists Eksport_Maskiner;    
-create table Eksport_Maskiner(
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+await db.query(`drop table if exists Eksport_Maskiner;
+create table Eksport_Maskiner (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating table Export_Medicin');
-await db.query(`
-drop table if exists Eksport_Medicin;    
-create table Eksport_Medicin(
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+await db.query(`drop table if exists Eksport_Medicin;
+create table Eksport_Medicin (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating table Export_Tobak');
-await db.query(`
-drop table if exists Export_Tobak;    
-create table Export_Tobak(
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+await db.query(`drop table if exists Eksport_Tobak;
+create table Eksport_Tobak (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating tables...');
-await db.query(`
-drop table if exists Import_Kaffe;
+await db.query(`drop table if exists Import_Kaffe;
 create table Import_Kaffe (
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating tables...');
-await db.query(`
-drop table if exists Import_Levende_Dyr;
-create table Import_Levende_Dyr(
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+await db.query(`drop table if exists Import_Levende_dyr;
+create table Import_Levende_dyr (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating tables...');
-await db.query(`
-drop table if exists Import_Maskiner;    
-create table Import_Maskiner(
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+await db.query(`drop table if exists Import_Maskiner;
+create table Import_Maskiner (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating tables...');
-await db.query(`
-drop table if exists Import_Medicin;
-create table Import_Medicin(
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+await db.query(`drop table if exists Import_Medicin;
+create table Import_Medicin (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
-console.log('Recreating tables...');
-await db.query(`
-drop table if exists Import_Tobaksvarer;
+await db.query(`drop table if exists Import_Tobaksvarer;
 create table Import_Tobaksvarer (
-sitc text,
-land text,
-indud text,
-tid integer,
-indhold integer
-)`
-);
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
+await db.query(`drop table if exists Import_Export;
+create table Import_Export (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
 
+await db.query(`drop table if exists Vigtigste_Handelspartnere;
+create table Vigtigste_Handelspartnere (
+    sitc text,
+    land text,
+    indud text,
+    tid integer,
+    indhold integer
+)`);
+
+// Upload CSV-data fra db/data-mappen
 await upload(
     db,
-    'db/Eksport_Kaffe.csv',
+    'db/data/Eksport_Kaffe.csv',
     'copy Eksport_Kaffe (sitc, land, indud, tid, indhold) from stdin with csv header'
 );
 
 await upload(
     db,
-    'db/Eksport_Levende_dyr.csv',
+    'db/data/Eksport_Levende_dyr.csv',
     'copy Eksport_Levende_dyr (sitc, land, indud, tid, indhold) from stdin with csv header'
-
 );
 
 await upload(
     db,
-    'db/Eksport_Maskiner.csv',
+    'db/data/Eksport_Maskiner.csv',
     'copy Eksport_Maskiner (sitc, land, indud, tid, indhold) from stdin with csv header'
 );
 
 await upload(
     db,
-    'db/Eksport_Medicin.csv',
-    'copy Export_Medicin (sitc, land, indud, tid, indhold) from stdin with csv header'
+    'db/data/Eksport_Medicin.csv',
+    'copy Eksport_Medicin (sitc, land, indud, tid, indhold) from stdin with csv header'
 );
 
 await upload(
     db,
-    'db/Eksport_Kaffe.csv',
-    'copy Eksport_Kaffe (sitc, land, indud, tid, indhold) from stdin with csv header'
-);
-
-
-await upload(
-    Data,
-    'Data/Import_Kaffe.csv',
-    'copy Import_Kaffe (SITC, Land, INDUD, TID, INDHOLD) from stdin with csv header'
+    'db/data/Eksport_Tobak.csv',
+    'copy Eksport_Tobak (sitc, land, indud, tid, indhold) from stdin with csv header'
 );
 
 await upload(
-    Data,
-    'Data/Import_Levende Dyr.csv',
-    'copy Import_Levende Dyr (SITC, Land, INDUD, TID, INDHOLD) from stdin with csv'
-
+    db,
+    'db/data/Import_Kaffe.csv',
+    'copy Import_Kaffe (sitc, land, indud, tid, indhold) from stdin with csv header'
 );
 
 await upload(
-    Data,
-    'Data/Import_Maskiner.csv',
-    'copy Import_Maskiner (SITC, Land, INDUD, TID, INDHOLD) from stdin with csv'
+    db,
+    'db/data/Import_Levende_dyr.csv',
+    'copy Import_Levende_dyr (sitc, land, indud, tid, indhold) from stdin with csv header'
 );
 
 await upload(
-    Data,
-    'Data/Import_Medicin.csv',
-    'copy Import_Medicin (SITC, Land, INDUD, TID, INDHOLD) from stdin with csv header'
+    db,
+    'db/data/Import_Maskiner.csv',
+    'copy Import_Maskiner (sitc, land, indud, tid, indhold) from stdin with csv header'
 );
 
 await upload(
-    Data,
-    'Data/Import_Tobaksvarer.csv',
-    'copy Import_Tobaksvarer (SITC, Land, INDUD, TID, INDHOLD) from stdin with csv header'
-
+    db,
+    'db/data/Import_Medicin.csv',
+    'copy Import_Medicin (sitc, land, indud, tid, indhold) from stdin with csv header'
 );
+
+await upload(
+    db,
+    'db/data/Import_Tobaksvarer.csv',
+    'copy Import_Tobaksvarer (sitc, land, indud, tid, indhold) from stdin with csv header'
+);
+
+await upload(
+    db,
+    'db/data/Import_Export.csv',
+    'copy Import_Export (sitc, land, indud, tid, indhold) from stdin with csv header'
+);
+
+await upload(
+    db,
+    'db/data/Vigtigste_Handelspartnere.csv',
+    'copy Vigtigste_Handelspartnere (sitc, land, indud, tid, indhold) from stdin with csv header'
+);
+
 console.log('Database and data ready');
 process.exit();
