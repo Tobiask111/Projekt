02955af84f26382ee834e6c0a31b8b9ed7da9b82
Tobiask111 <eaa24tkrin@students.eaaa.dk>
2025-05-21 19:46:23 +0200

    Added comments to ButtonsForSite

--- frontend/ButtonsForSite.js
+++ frontend/ButtonsForSite.js
@@ -1,6 +1,7 @@
+//finds the html element with the id "worldmapknap" and adds the addeventlistener to listen for when the button is pressed
 document.getElementById("worldmapknap").addEventListener("click", function () {
-  const sektion = document.getElementById("tagmigtilworldmap");
-  sektion.scrollIntoView({ behavior: "smooth" });
+  const sektion = document.getElementById("tagmigtilworldmap"); //saves the element with the id "tagmigtilworldmap" to the varible sektion
+  sektion.scrollIntoView({ behavior: "smooth" }); //makes it so that when pressed it scrolls down to the corresponding section after. And it does it smoothly instead of jumping right to it
 });
 
 document.getElementById("barchartknap").addEventListener("click", function () {
